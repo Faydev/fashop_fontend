718b40440caf5384b533f246192a493f606c6e4e
Olúwatìmìlé̩hìn Ako̩gun <hello@timiakogun.com>
2019-11-05 18:34:07 +0100

    validator added

--- js/main.js
+++ js/main.js
@@ -1,40 +1,23 @@
+"use strict";
 
-var inputName = document.getElementById("input-name");
-var inputMail = document.getElementById("input-mail");
-var inputPassword = document.getElementById("input-password");
-var inputConfirmpassword = document.getElementById("input-confirmpassword");
-var button = document.querySelector(".btn");
-var regEx = /\S+@\S+\.\S+/;
+var Validator = function(validationCall, errorCallBack) {
+  this.validate = function() {
+    if (!validationCall()) {
+      return errorCallBack();
+    }
+  };
+};
 
-function checkInput() {
-    if (inputName.value.trim()!== "" && regEx.test(inputMail.value)) {
-        button.style.backgroundColor= "#ffffff";
+function AccountFormValidation() {
+  this.fieldValidators = {};
+  this.addField = function(fieldName) {
+    this.fieldValidators[fieldName] = [];
+  };
+  this.addValidationToField = function(fieldName, validator) {
+    if (this.fieldValidators[fieldName] == null) {
+      this.addField(fieldName);
+    } else {
+      this.fieldValidators[fieldName].push(validator);
     }
-    else{
-        button.style.backgroundColor= "#1641FF"
-    }
+  };
 }
-
-
-// var $ipp = document.querySelector("#ipp").value;
-// var $email = document.querySelector("#email").value;
-// var $cpp = document.querySelector("#cpp").value;
-// var regEx = /\S@\S+\.\S+/;
-
-//     if($ipn==""||$ipp==""){
-//         alert("fill all empty field");
-//     //return false;
-//     }else if(ipp!=$cpp){
-//         alert("password does not match");
-
-//     }else if($email=="" && regEx.test(email.value)){
-//         alert("email field is not filled");
-
-//     }
-    
-//     else{
-//     alert(" login successful");
-//     //return true;
-//     }
-
-// }
